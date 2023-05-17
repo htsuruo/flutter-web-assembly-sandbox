82b8a07432cdeace9ca0f01f2bc214e7bf6fd392
tsuruo <59546923+tsuruobiz@users.noreply.github.com>
2023-05-17 17:57:51 +0900

    step3: Complete painting on scene

--- lib/title_screen/title_screen.dart
+++ lib/title_screen/title_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 import '../assets.dart';
+import '../styles.dart'; // Add this import
 
 class TitleScreen extends StatelessWidget {
   const TitleScreen({super.key});
 
+  final _finalReceiveLightAmt = 0.7; // Add this attribute
+  final _finalEmitLightAmt = 0.5; // And this attribute
+
   @override
   Widget build(BuildContext context) {
+    final orbColor = AppColors.orbColors[0]; // Add this final variable
+    final emitColor = AppColors.emitColors[0]; // And this one
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
@@ -16,28 +23,82 @@ class TitleScreen extends StatelessWidget {
             Image.asset(AssetPaths.titleBgBase),
 
             /// Bg-Receive
-            Image.asset(AssetPaths.titleBgReceive),
+            _LitImage(
+              // Modify from here...
+              color: orbColor,
+              imgSrc: AssetPaths.titleBgReceive,
+              lightAmt: _finalReceiveLightAmt,
+            ), // to here.
 
             /// Mg-Base
-            Image.asset(AssetPaths.titleMgBase),
+            _LitImage(
+              // Modify from here...
+              imgSrc: AssetPaths.titleMgBase,
+              color: orbColor,
+              lightAmt: _finalReceiveLightAmt,
+            ), // to here.
 
             /// Mg-Receive
-            Image.asset(AssetPaths.titleMgReceive),
+            _LitImage(
+              // Modify from here...
+              imgSrc: AssetPaths.titleMgReceive,
+              color: orbColor,
+              lightAmt: _finalReceiveLightAmt,
+            ), // to here.
 
             /// Mg-Emit
-            Image.asset(AssetPaths.titleMgEmit),
+            _LitImage(
+              // Modify from here...
+              imgSrc: AssetPaths.titleMgEmit,
+              color: emitColor,
+              lightAmt: _finalEmitLightAmt,
+            ), // to here.
 
             /// Fg-Rocks
             Image.asset(AssetPaths.titleFgBase),
 
             /// Fg-Receive
-            Image.asset(AssetPaths.titleFgReceive),
+            _LitImage(
+              // Modify from here...
+              imgSrc: AssetPaths.titleFgReceive,
+              color: orbColor,
+              lightAmt: _finalReceiveLightAmt,
+            ), // to here.
 
             /// Fg-Emit
-            Image.asset(AssetPaths.titleFgEmit),
+            _LitImage(
+              // Modify from here...
+              imgSrc: AssetPaths.titleFgEmit,
+              color: emitColor,
+              lightAmt: _finalEmitLightAmt,
+            ), // to here.
           ],
         ),
       ),
     );
   }
 }
+
+class _LitImage extends StatelessWidget {
+  // Add from here...
+  const _LitImage({
+    required this.color,
+    required this.imgSrc,
+    required this.lightAmt,
+  });
+  final Color color;
+  final String imgSrc;
+  final double lightAmt;
+
+  @override
+  Widget build(BuildContext context) {
+    final hsl = HSLColor.fromColor(color);
+    return ColorFiltered(
+      colorFilter: ColorFilter.mode(
+        hsl.withLightness(hsl.lightness * lightAmt).toColor(),
+        BlendMode.modulate,
+      ),
+      child: Image.asset(imgSrc),
+    );
+  }
+}                                                         // to here.
