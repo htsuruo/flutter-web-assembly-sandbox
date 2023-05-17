c5d330f9fde46b1d5a8b00d613a94919a8974f72
tsuruo <59546923+tsuruobiz@users.noreply.github.com>
2023-05-17 18:03:06 +0900

    step4: Complete UI

--- lib/title_screen/title_screen_ui.dart
+++ lib/title_screen/title_screen_ui.dart
@@ -48,6 +48,17 @@ class TitleScreenUi extends StatelessWidget {
               ),
             ),
           ), // to here.
+          /// StartBtn
+          BottomRight(
+            // Add from here...
+            child: UiScaler(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20, right: 40),
+                child: _StartBtn(onPressed: () {}),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -182,6 +193,58 @@ class _TitleText extends StatelessWidget {
         ),
         Text('INTO THE UNKNOWN', style: TextStyles.h3),
       ],
+    );
+  }
+}
+
+class _StartBtn extends StatefulWidget {
+  const _StartBtn({required this.onPressed});
+  final VoidCallback onPressed;
+
+  @override
+  State<_StartBtn> createState() => _StartBtnState();
+}
+
+class _StartBtnState extends State<_StartBtn> {
+  AnimationController? _btnAnim;
+  bool _wasHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return FocusableControlBuilder(
+      cursor: SystemMouseCursors.click,
+      onPressed: widget.onPressed,
+      builder: (_, state) {
+        if ((state.isHovered || state.isFocused) &&
+            !_wasHovered &&
+            _btnAnim?.status != AnimationStatus.forward) {
+          _btnAnim?.forward(from: 0);
+        }
+        _wasHovered = (state.isHovered || state.isFocused);
+        return SizedBox(
+          width: 520,
+          height: 100,
+          child: Stack(
+            children: [
+              Positioned.fill(child: Image.asset(AssetPaths.titleStartBtn)),
+              if (state.isHovered || state.isFocused) ...[
+                Positioned.fill(
+                    child: Image.asset(AssetPaths.titleStartBtnHover)),
+              ],
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('START MISSION',
+                        style: TextStyles.btn
+                            .copyWith(fontSize: 24, letterSpacing: 18)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
